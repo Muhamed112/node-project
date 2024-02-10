8235eef4c12f83fa3e89fe0ab430f50e29d78722
Muhamed112 <54117777+Muhamed112@users.noreply.github.com>
2024-02-10 11:40:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Require http module
+import http from "http";
+// Create server
+http
+  .createServer(function (req, res) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    // Send the response body as "This is the example
+    // of node.js web based application"
+    res.end("Example of node.js web-based application");
+
+    // Console will display the message
+  })
+  .listen(5000, () => console.log("Server running at http://127.0.0.1:5000/"));
